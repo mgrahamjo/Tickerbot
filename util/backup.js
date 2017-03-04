60d8a3b9a30b073c7ca0bb0ea34eeb5316bc4e96
Mike <mgrahamjo@gmail.com>
2017-03-04 14:09:08 -0800

    make cronitor ID an command line arg

--- util/backup.js
+++ util/backup.js
@@ -1,23 +1,32 @@
-const spawn = require('child_process').spawn;
+const spawn = require('child_process').spawn,
+    cronitor = process.argv[2];
 
 module.exports = () => new Promise(resolve => {
 
-    spawn('git', ['commit', '-am', 'Auto backup ' + new Date().toJSON().substring(0, 10)], {
-        stdio: 'inherit'
-    }).on('close', code => {
+    if (cronitor) {
 
-        if (code === 0) {
+        spawn('git', ['commit', '-am', 'Auto backup ' + new Date().toJSON().substring(0, 10)], {
+            stdio: 'inherit'
+        }).on('close', code => {
 
-            spawn('git', ['push'], {
-                stdio: 'inherit'
-            }).on('close', resolve);
+            if (code === 0) {
 
-        } else {
+                spawn('git', ['push'], {
+                    stdio: 'inherit'
+                }).on('close', resolve);
 
-            resolve();
+            } else {
 
-        }
+                resolve();
 
-    });
+            }
+
+        });
+
+    } else {
+
+        resolve();
+        
+    }
 
 });
